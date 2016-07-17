d5765736810107bc18e8e99db5de6b3c4c731875
Offirmo <offirmo.net@gmail.com>
2016-07-17 22:59:04 +0000

    +

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,9 @@
 import * as _ from 'lodash'
 
-//console.log('Hello from', __filename)
+// Use the ES delimiter as an alternative to the default "interpolate" delimiter.
+const default_template = _.template('hello, ${ target }!')
 
-export type LogFn = typeof console.log
-
-// options when creating the logger
-export interface PrefixedLoggerOptions {
-	logFn?: LogFn
-	spacerAlt?: string
-	spacer?: string
-	isEnabled?: (() => boolean) | boolean
-}
-
-// internal logger options
-export interface PrefixedLoggerParams {
-	logFn: LogFn
-	spacerAlt: string
-	spacer: string
-	prefix: () => string
-	isEnabled: () => boolean
-}
-
-// http://stackoverflow.com/questions/12766528/build-a-function-object-with-properties-in-typescript
-export interface PrefixedLogger {
-	(...rest: any[]): void
-	options: PrefixedLoggerParams
-	__src: string // special string for module consumption experiments
-}
-
-export default function makePrefixedLogger(
-	prefix: Function | string,
-	logFnParam?: LogFn | PrefixedLoggerOptions,
-	optionsParam?: PrefixedLoggerOptions
-): PrefixedLogger
+export default function hello(target: string = 'World'): void
 {
-	if (_.isObject(logFnParam) && !_.isFunction(logFnParam))
-		[logFnParam, optionsParam] = [undefined, logFnParam]
-	optionsParam = optionsParam || {}
-
-	const options: PrefixedLoggerParams = {
-		logFn: logFnParam || optionsParam.logFn || console.log.bind(console),
-		spacerAlt: optionsParam.spacerAlt || optionsParam.spacer || '',
-		spacer: optionsParam.spacer || ' ',
-		prefix: (_.isFunction(prefix) ? prefix : () => prefix) as () => string,
-		isEnabled: (
-			_.isFunction(optionsParam.isEnabled) ?
-			optionsParam.isEnabled :
-			(_.isBoolean(optionsParam.isEnabled) ? (() => optionsParam.isEnabled) : (() => true))
-		) as () => boolean
-	}
-
-	const logger: PrefixedLogger = <PrefixedLogger>function log(param1: any, ...rest: any[]) {
-		if (!options.isEnabled()) return
-
-		if (_.isString(param1))
-			options.logFn(options.prefix() + options.spacer + param1, ...rest)
-		else
-			options.logFn(options.prefix() + options.spacerAlt, param1, ...rest)
-	}
-	logger.options = options
-	logger.__src = '???' // don't mind this
-
-	return logger
+	console.log(default_template({target}))
 }
--- test/index.ts
+++ test/index.ts
@@ -3,19 +3,7 @@
 
 /// <reference path="../node_modules/@types/node/index.d.ts" />
 
-import makePrefixedLogger from '../src'
+import hello from '../src'
 
-const logger1 = makePrefixedLogger('* [foo]')
-const logger2 = makePrefixedLogger(() => `[${Date.now()}]`, {
-	spacer: ' > ',
-	spacerAlt: ' >'
-})
-const logger3 = makePrefixedLogger('!!!', console.error.bind(console), {
-	isEnabled: false
-})
-
-logger1('Hello using version : "%s"', logger1.__src)
-logger2('Hello again')
-logger1({foo: 'bar'})
-logger2([ 'foo', 'bar'])
-logger3('should not appear')
+hello()
+hello('Yves')
